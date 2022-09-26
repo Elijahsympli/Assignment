9271d9d0b2b1a42cb0f9b3b2dfa11782080f4f1b
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-26 19:28:15 +0530

    Create AddNo.java

---
+++ 26-09-2022/AddNo.java
@@ -0,0 +1,16 @@
+//java program to add two number
+package com.pack;
+
+import java.util.Scanner;
+
+public class AddNo {
+public static void main(String args[]) {
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter two number to add");
+	int n1=sc.nextInt();
+	int n2=sc.nextInt();
+	System.out.println(n1+n2);
+}
+
+}
